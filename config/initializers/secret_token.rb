9ef7cb13fea8e5988f097bd8b920d1b3689e7416
Marius Rieder <marius.rieder@durchmesser.ch>
2014-02-09 09:39:52 +0100

    Initial rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Varnishworkbench::Application.config.secret_key_base = 'e09c50a3e8cae314cb13e04400b86a1ab0b9c36f7b40beeae3f167180dabc13e6e66cc67caf5f83304d8c47447225166d3ef82c6aa691f29c0c91eff3d473c6c'
