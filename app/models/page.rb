4c0fce9c889c4d7af7816d5cc99a3a458e6d6bf0
Marius Rieder <marius.rieder@durchmesser.ch>
2014-02-09 09:48:48 +0100

    Add defaults for Page model

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,10 @@
 class Page < ActiveRecord::Base
+
+    after_initialize :init
+
+    def init
+        self.title ||= 'Varnish Workbench'
+        self.text ||= 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren.'
+    end
+
 end
