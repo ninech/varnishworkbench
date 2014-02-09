92c19fa89801fbc30c9392ea93ae5a67ec32915a
Marius Rieder <marius.rieder@durchmesser.ch>
2014-02-09 12:05:42 +0100

    Add defaults for CacheControl Model

--- app/models/cache_control.rb
+++ app/models/cache_control.rb
@@ -1,2 +1,16 @@
 class CacheControl < ActiveRecord::Base
+
+    after_initialize :init
+
+    def init
+        self.expire  ||= 'now'
+        self.noCache ||= false
+        self.noStore ||= false
+        self.public  ||= false
+        self.private ||= false
+        self.maxAge  ||= 0
+        self.sMaxAge ||= 0
+        self.timeout ||= 1
+    end
+
 end
