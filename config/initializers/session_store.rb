9ac7eda4d684155d511051760168f011e95ed108
Marius Rieder <marius.rieder@durchmesser.ch>
2014-02-12 12:23:37 +0100

    No session cookies please

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Varnishworkbench::Application.config.session_store :cookie_store, key: '_varnishworkbench_session'
+Varnishworkbench::Application.config.session_store :disabled
